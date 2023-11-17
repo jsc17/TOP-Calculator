13103f97e01adfecf2306696f4710b5ad8eed6be
Jonathan Colton <jonathan.colton@yahoo.com>
2023-11-16 23:15:26 -0500

    Create function outline and basic math functions

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let first, second, operator;
+
+function add(first, second) {
+  return first + second;
+}
+
+function subtract(first, second) {
+  return first - second;
+}
+
+function multiply(first, second) {
+  return first * second;
+}
+
+function divide(first, second) {
+  if (second == 0) return "Divide by Zero error";
+  return first / second;
+}
+
+function clear() {}
+
+function equals() {}
+
+function onClickNumber() {}
+
+function onClickOperator() {}
