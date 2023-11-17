ac734d57e7c1c1ec8da35a3c75ae2dd1461e940c
Jonathan Colton <jonathan.colton@yahoo.com>
2023-11-17 09:43:41 -0500

    Add display text and additional styling

--- scripts.js
+++ scripts.js
@@ -1,20 +1,25 @@
-let first, second, operator;
+let first = "",
+  second = "",
+  operator = "";
 
-function add(first, second) {
-  return first + second;
+const history = document.querySelector("#history");
+const current = document.querySelector("#current");
+
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(first, second) {
-  return first - second;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(first, second) {
-  return first * second;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(first, second) {
-  if (second == 0) return "Divide by Zero error";
-  return first / second;
+function divide(num1, num2) {
+  if (num2 == 0) return "Divide by Zero error";
+  return num1 / num2;
 }
 
 function clear() {}
