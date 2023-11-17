2306fb5984fc8a79474ed44007e0f7495189d048
Jonathan Colton <jonathan.colton@yahoo.com>
2023-11-17 10:16:49 -0500

    Add number and clear button functionality Added logic switch and clear functions to write to display and clear it

--- scripts.js
+++ scripts.js
@@ -22,13 +22,49 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function clear() {}
+function clear() {
+  historyDisplay.innerText = "";
+  currentDisplay.innerHTML = "0";
+}
 
-function del() {}
+function del() {
+  currentDisplay.innerText = currentDisplay.innerText.substring(
+    0,
+    currentDisplay.innerText.length - 1
+  );
+  if (currentDisplay.innerText.length == 0) currentDisplay.innerText = "0";
+}
 
 function equals() {}
 
-function onClick(target) {}
+function onClick(target) {
+  switch (target) {
+    case "DEL":
+      del();
+      break;
+    case "CE":
+      clear();
+      break;
+    case "=":
+      equals();
+      break;
+    case ".":
+      break;
+    case "+":
+    case "-":
+    case "/":
+    case "X":
+      break;
+    // default case catches any numbers pressed to add to the current value
+    default:
+      if (currentDisplay.innerText == "0") {
+        currentDisplay.innerText = target;
+      } else {
+        currentDisplay.innerText += target;
+      }
+      break;
+  }
+}
 
 keyButtons.forEach((button) => {
   button.addEventListener("click", function (e) {
